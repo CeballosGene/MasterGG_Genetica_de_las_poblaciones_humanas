6d41eea359a289ac5cc07b9cdd0cf27b0490539e
CeballosGene <42999364+CeballosGene@users.noreply.github.com>
2018-11-05 12:48:29 +0100

    Update Analisis Componentes Principales.R

--- Mandibula Habsburgo/Analisis Componentes Principales.R
+++ Mandibula Habsburgo/Analisis Componentes Principales.R
@@ -14,29 +14,30 @@ md <- read.csv("~/md.csv", sep=";")
 mp <- read.csv("~/mp.csv", sep=";")
 
 #Preparo los datos
-Fped<-md[,12]
-md1<-md[,1:11]
+Fped<-mp[,12]
+mp1<-mp[,1:11]
 class(Fped)
 Fped=as.factor(Fped)
 ##############################################################################
 
 #Analisis de Componentes Principales
-pca.md<-princomp(md1, cor = TRUE, scores = TRUE, scale=FALSE)
+pca.mp<-princomp(mp1, cor = TRUE, scores = TRUE, scale=FALSE)
 ??pricomp #scale=FALSE: the variable standard deviations (the scaling applied to each variable )
+#Scores: coordinates on each principal component
 
 #Scores
-ind.scores <- pca.md$scores
+ind.scores <- pca.mp$scores
 ind.scores[,1:3]
-pca.md$loadings
+pca.mp$loadings #the matrix of variable loadings (columns are eigenvectors, i.e. rotations)
 mean(ind.scores[,1])
 var(ind.scores[,1])
 
 #Eigenvalues
-get_eigenvalue(pca.md)
-Eigen <- eigen(cor(md1))
+get_eigenvalue(pca.mp)
+Eigen <- eigen(cor(mp1))
 
 # Resultados para las Variables!
-var <- get_pca_var(pca.md)
+var <- get_pca_var(pca.mp)
 var
 
 #Coordenadas de las variables para las 4 primeras componentes
@@ -46,8 +47,8 @@ var$coord[,1:4] #esto es: correlacion entre las variables y los componentes
 var_cor_func <- function(var.loadings, comp.sdev){
   var.loadings*comp.sdev
 }
-loadings <- pca.md$loadings
-sdev <- pca.md$sdev
+loadings <- pca.mp$loadings
+sdev <- pca.mp$sdev
 var.coord <- var.cor <- t(apply(loadings, 1, var_cor_func, sdev))
 head(var.coord[, 1:4])
 ##########################################################################
@@ -58,30 +59,30 @@ head(var.coord[, 1:4])
 
 #Eigenvalues
 windows()
-fviz_screeplot(pca.md,choice="eigenvalue")
+fviz_screeplot(pca.mp,choice="eigenvalue")
 ###########################
 
 #INDIVIDUOS
 windows()
-fviz_pca_ind(pca.md)+labs(title ="PCA", x = "PC1", y = "PC2")
+fviz_pca_ind(pca.mp)+labs(title ="PCA", x = "PC1", y = "PC2")
 
 windows()
-fviz_pca_ind(pca.md, col.ind="cos2")
+fviz_pca_ind(pca.mp, col.ind="cos2")
 # cos2 = calidad de los individuos en el mapa factor
 windows()
-fviz_pca_ind(pca.md, col.ind="cos2") +
+fviz_pca_ind(pca.mp, col.ind="cos2") +
   scale_color_gradient2(low="white", mid="blue",
                         high="red", midpoint=0.5)
 # Color por la contribucion individual
 windows()
-fviz_pca_ind(pca.md, col.ind="contrib") +
+fviz_pca_ind(pca.mp, col.ind="contrib") +
   scale_color_gradient2(low="white", mid="blue",
                         high="red", midpoint=5)
 # Color por grupo de consanguinidad
 windows()
-fviz_pca_ind(pca.md, label="none", habillage=md$F)
+fviz_pca_ind(pca.mp, label="none", habillage=md$F)
 windows()
-p <- fviz_pca_ind(pca.md, label="none", habillage=md$F,
+p <- fviz_pca_ind(pca.mp, label="none", habillage=mp$F,
                   addEllipses=TRUE, ellipse.level=0.95)
 p + scale_color_brewer(palette="Dark2") +
   theme_minimal()
@@ -91,15 +92,15 @@ print(p)
 #VARIABLES!
 #Correlacion entre variables y componentes principales como coordenadas:
 windows()
-fviz_pca_var(pca.md)
+fviz_pca_var(pca.mp)
 windows()
-fviz_pca_var(pca.md, geom = c("point", "text"))
+fviz_pca_var(pca.mp, geom = c("point", "text"))
 
 #value of cos2. The contribution of a variable to a given principal component is (in percentage)
 
 #Contribucion de la variable a la componente principal
 windows()
-fviz_pca_var(pca.md, col.var="contrib")+
+fviz_pca_var(pca.mp, col.var="contrib")+
   scale_color_gradient2(low="white", mid="blue", 
                         high="red", midpoint=7) + theme_minimal()
 #valor de cos2: contribucion de la variable al componente principal correspondiente.
@@ -108,14 +109,34 @@ var$cos2[,1:4]
 
 #VARIABLES E INDIVIDUOS
 windows()
-fviz_pca_biplot(pca.md, label ="var")
+fviz_pca_biplot(pca.mp, label ="var")
 windows()
-fviz_pca_biplot(pca.md, label ="ind")
+fviz_pca_biplot(pca.mp, label ="ind")
 windows()
-fviz_pca_biplot(pca.md, label =c("ind","var"))
+fviz_pca_biplot(pca.mp, label =c("ind","var"))
 
 windows()
-fviz_pca_biplot(pca.md, label="var", habillage=md$F,
+fviz_pca_biplot(pca.mp, label="var", habillage=md$F,
                 addEllipses=TRUE, ellipse.level=0.95)
 
-biplot(pca.md)
+biplot(pca.mp)
+
+########################################################
+#Regression F with MP
+
+ind.coord.mp<-pca.mp$scores[,1:3]
+id<-1:16
+a1.mp<-cbind(ind.coord.mp,id)
+a2<-cbind(mp$Inb,id);colnames(a2)=c("inb","id")
+inb.dep.mp<-merge(a1.mp,a2,by="id")
+
+pca1.mp<-lm(Comp.1 ~ inb, data = inb.dep.mp)
+summary(pca1.mp)
+
+pca2.mp<-lm(Comp.2 ~ inb, data = inb.dep.mp)
+summary(pca2.mp)
+
+
+
+
+
